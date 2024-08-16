85deb302be74e641c79f22da3ebc232af20bb5dc
Viet Ngoc <ngocjohn@me.com>
2024-08-16 19:33:01 +0200

    chore: Update .gitignore and add missing translations for Indonesian language

--- src/localize/languageImports.ts
+++ src/localize/languageImports.ts
@@ -1,19 +1,25 @@
 // This file is generated automatically by the generate-lang-imports script. Do not modify it manually.
 
 import * as cs from '../languages/cs.json';
+import * as da from '../languages/da.json';
 import * as de from '../languages/de.json';
 import * as en from '../languages/en.json';
+import * as id from '../languages/id.json';
 
 const languages: any = {
   cs: cs,
+  da: da,
   de: de,
   en: en,
+  id: id,
 };
 
 export const languageOptions = [
   { key: 'cs', name: cs.name, nativeName: cs.nativeName },
+  { key: 'da', name: da.name, nativeName: da.nativeName },
   { key: 'de', name: de.name, nativeName: de.nativeName },
   { key: 'en', name: en.name, nativeName: en.nativeName },
+  { key: 'id', name: id.name, nativeName: id.nativeName },
 ];
 
 export { languages };
