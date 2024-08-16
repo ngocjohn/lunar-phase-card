3df64a11f9381f09e9c3e137fbdf0e5ed889d272
Viet Ngoc <ngocjohn@me.com>
2024-08-16 04:23:09 +0200

    feat: Add German (deutsch) translation support

--- src/localize/languageImports.ts
+++ src/localize/languageImports.ts
@@ -1,15 +1,18 @@
 // This file is generated automatically by the generate-lang-imports script. Do not modify it manually.
 
 import * as cs from '../languages/cs.json';
+import * as de from '../languages/de.json';
 import * as en from '../languages/en.json';
 
 const languages: any = {
   cs: cs,
+  de: de,
   en: en,
 };
 
 export const languageOptions = [
   { key: 'cs', name: cs.name, nativeName: cs.nativeName },
+  { key: 'de', name: de.name, nativeName: de.nativeName },
   { key: 'en', name: en.name, nativeName: en.nativeName },
 ];
 
