f3e2e9d433a7c776a662a1a9a5918b827419d677
Viet Ngoc <ngocjohn@me.com>
2024-08-19 09:12:42 +0200

    chore: Update language imports to include Portuguese translation

--- src/localize/languageImports.ts
+++ src/localize/languageImports.ts
@@ -5,6 +5,7 @@ import * as da from '../languages/da.json';
 import * as de from '../languages/de.json';
 import * as en from '../languages/en.json';
 import * as id from '../languages/id.json';
+import * as pt from '../languages/pt.json';
 
 const languages: any = {
   cs: cs,
@@ -12,6 +13,7 @@ const languages: any = {
   de: de,
   en: en,
   id: id,
+  pt: pt,
 };
 
 export const languageOptions = [
@@ -20,6 +22,7 @@ export const languageOptions = [
   { key: 'de', name: de.name, nativeName: de.nativeName },
   { key: 'en', name: en.name, nativeName: en.nativeName },
   { key: 'id', name: id.name, nativeName: id.nativeName },
+  { key: 'pt', name: pt.name, nativeName: pt.nativeName },
 ];
 
 export { languages };
