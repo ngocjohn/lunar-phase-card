77f20d5f0080bccdd964415cced7076bf101acc3
Viet Ngoc <ngocjohn@me.com>
2024-09-05 09:04:46 +0200

    Update Italian language support

--- src/localize/languageImports.ts
+++ src/localize/languageImports.ts
@@ -7,9 +7,10 @@ import * as de from '../languages/de.json';
 import * as en from '../languages/en.json';
 import * as fr from '../languages/fr.json';
 import * as id from '../languages/id.json';
+import * as it from '../languages/it.json';
 import * as pt from '../languages/pt.json';
-import * as sk from '../languages/sk.json';
 import * as ru from '../languages/ru.json';
+import * as sk from '../languages/sk.json';
 
 const languages: any = {
   ca: ca,
@@ -19,9 +20,10 @@ const languages: any = {
   en: en,
   fr: fr,
   id: id,
+  it: it,
   pt: pt,
-  sk: sk,
   ru: ru,
+  sk: sk,
 };
 
 export const languageOptions = [
@@ -32,9 +34,10 @@ export const languageOptions = [
   { key: 'en', name: en.name, nativeName: en.nativeName },
   { key: 'fr', name: fr.name, nativeName: fr.nativeName },
   { key: 'id', name: id.name, nativeName: id.nativeName },
+  { key: 'it', name: it.name, nativeName: it.nativeName },
   { key: 'pt', name: pt.name, nativeName: pt.nativeName },
-  { key: 'sk', name: sk.name, nativeName: sk.nativeName },
   { key: 'ru', name: ru.name, nativeName: ru.nativeName },
+  { key: 'sk', name: sk.name, nativeName: sk.nativeName },
 ];
 
 export { languages };
