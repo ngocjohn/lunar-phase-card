894cea28f9f68c07a489062ea6bdf4c1c1f51041
Viet Ngoc <ngocjohn@me.com>
2024-10-25 05:36:16 +0200

    refactor: Update moon images and paths for improved organization and readability

--- src/const.ts
+++ src/const.ts
@@ -5,28 +5,43 @@ export const REPOSITORY = repository.repo;
 export const CARD_VERSION = `v${version}`;
 export const BASE_REFRESH_INTERVAL = 60 * 1000; // 1 minute
 
-import moon_phase_0 from './images/moon_phase_0.png';
-import moon_phase_1 from './images/moon_phase_1.png';
-import moon_phase_2 from './images/moon_phase_2.png';
-import moon_phase_3 from './images/moon_phase_3.png';
-import moon_phase_4 from './images/moon_phase_4.png';
-import moon_phase_5 from './images/moon_phase_5.png';
-import moon_phase_6 from './images/moon_phase_6.png';
-import moon_phase_7 from './images/moon_phase_7.png';
-import moon_phase_8 from './images/moon_phase_8.png';
-import moon_phase_9 from './images/moon_phase_9.png';
-import moon_phase_10 from './images/moon_phase_10.png';
-import moon_phase_11 from './images/moon_phase_11.png';
-import moon_phase_12 from './images/moon_phase_12.png';
-import moon_phase_13 from './images/moon_phase_13.png';
-import moon_phase_14 from './images/moon_phase_14.png';
-import moon_phase_15 from './images/moon_phase_15.png';
 import background from './images/bkg.png';
+
+import moon_phase_1 from './images/moon_pic/moon_image_00001.png';
+import moon_phase_2 from './images/moon_pic/moon_image_00002.png';
+import moon_phase_3 from './images/moon_pic/moon_image_00003.png';
+import moon_phase_4 from './images/moon_pic/moon_image_00004.png';
+import moon_phase_5 from './images/moon_pic/moon_image_00005.png';
+import moon_phase_6 from './images/moon_pic/moon_image_00006.png';
+import moon_phase_7 from './images/moon_pic/moon_image_00007.png';
+import moon_phase_8 from './images/moon_pic/moon_image_00008.png';
+import moon_phase_9 from './images/moon_pic/moon_image_00009.png';
+import moon_phase_10 from './images/moon_pic/moon_image_00010.png';
+import moon_phase_11 from './images/moon_pic/moon_image_00011.png';
+import moon_phase_12 from './images/moon_pic/moon_image_00012.png';
+import moon_phase_13 from './images/moon_pic/moon_image_00013.png';
+import moon_phase_14 from './images/moon_pic/moon_image_00014.png';
+import moon_phase_15 from './images/moon_pic/moon_image_00015.png';
+import moon_phase_16 from './images/moon_pic/moon_image_00016.png';
+import moon_phase_17 from './images/moon_pic/moon_image_00017.png';
+import moon_phase_18 from './images/moon_pic/moon_image_00018.png';
+import moon_phase_19 from './images/moon_pic/moon_image_00019.png';
+import moon_phase_20 from './images/moon_pic/moon_image_00020.png';
+import moon_phase_21 from './images/moon_pic/moon_image_00021.png';
+import moon_phase_22 from './images/moon_pic/moon_image_00022.png';
+import moon_phase_23 from './images/moon_pic/moon_image_00023.png';
+import moon_phase_24 from './images/moon_pic/moon_image_00024.png';
+import moon_phase_25 from './images/moon_pic/moon_image_00025.png';
+import moon_phase_26 from './images/moon_pic/moon_image_00026.png';
+import moon_phase_27 from './images/moon_pic/moon_image_00027.png';
+import moon_phase_28 from './images/moon_pic/moon_image_00028.png';
+import moon_phase_29 from './images/moon_pic/moon_image_00029.png';
+import moon_phase_30 from './images/moon_pic/moon_image_00030.png';
+import moon_phase_31 from './images/moon_pic/moon_image_00031.png';
 
 export const BACKGROUND = background;
 
 export const MOON_IMAGES = [
-  moon_phase_0,
   moon_phase_1,
   moon_phase_2,
   moon_phase_3,
@@ -42,6 +57,22 @@ export const MOON_IMAGES = [
   moon_phase_13,
   moon_phase_14,
   moon_phase_15,
+  moon_phase_16,
+  moon_phase_17,
+  moon_phase_18,
+  moon_phase_19,
+  moon_phase_20,
+  moon_phase_21,
+  moon_phase_22,
+  moon_phase_23,
+  moon_phase_24,
+  moon_phase_25,
+  moon_phase_26,
+  moon_phase_27,
+  moon_phase_28,
+  moon_phase_29,
+  moon_phase_30,
+  moon_phase_31,
 ];
 
 export const FONTSIZES: FontSizeOptions[] = ['auto', 'small', 'medium', 'large', 'x-large', 'xx-large'];
